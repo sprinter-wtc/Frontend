aba004c8264f1f9dc2268517f4a691c5d969c4d9
Taenggu99 <leetaeeun99@gmail.com>
2025-11-24 16:45:57 +0900

    fix: 카페 리뷰목록 api 수정 #4

--- src/api/reviewApi.ts
+++ src/api/reviewApi.ts
@@ -1,15 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "https://test.studyspot.kr/api";
+const BASE_URL = "https://studyspot.kr/api";
 
-// 베스트 리뷰
 export const getBestReviews = async (cafeId: number) => {
   const response = await axios.get(`${BASE_URL}/reviews/best/${cafeId}`);
-  return response.data.data;
-};
-
-// 전체 리뷰
-export const getReviews = async (cafeId: number) => {
-  const response = await axios.get(`${BASE_URL}/reviews/${cafeId}`);
-  return response.data.data;
+  return response.data.data; 
 };
